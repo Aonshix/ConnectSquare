7e2093d3483223fd09eb3e2001af4faf8efc413a
Aonshix <106062874+Aonshix@users.noreply.github.com>
2022-06-21 20:29:50 +1000

    Task # 2 Complete Need to clean up and comment for submission.

--- ConnectSquare_Task_2.py
+++ ConnectSquare_Task_2.py
@@ -1,46 +1,68 @@
-# def is_winner(board, coin):
-#     """
-#     This function searches for winning squares (a 2x2 square of spaces containing just a single colour of coin),
-#     returning True if this is the case for the colour of (coin) or False if there is no such valid square.
-#     """
-#     if:
-#         return True
-#
-#     else:
-#         return False
+# def is_winner(board, coin): """ This function searches for winning squares (a 2x2 square of spaces containing just # a single colour of coin), returning True if this is the case for the colour of (coin) or False if there is no such valid square. This is actually quite close to working, I just have to figure out how to get the loop to only operate within the right index. """
 def check_for_two(board, coin):
-    rows = len(board)
-    print(f"The board has {rows} rows")
+    found_horizontals = {
+        -1: [0,0]
+    }
     row_count = -1
-    print(coin)
+    rows = len(board)
+    # print(f"The board has {rows} rows")
+    row_columns = len(board[0])
+    # print(f" Row columns = {row_columns}")
+    # print(f"The user's coin is {coin}")
+    for x in range(rows):
+        found_horizontals[x] = [""]
     for row in board:
         row_count += 1
-        row_length = len(row)
-        print(f" Row length = {row_length}")
+        # print(f"Increased row count to: {row_count}")
+        # print(board[row_count])
         column = -1
-        short_row_length = row_length - 2
-        while column <= short_row_length:
-            for value in row:
-                print(f"Value = {value}")
-                if value == coin and row[column + 2] == coin:
-                    column += 1
-                    print(f"Column = {column}")
-                    print(f"Function has detected {value} and {row[column +1]} are a match!")
-                    print(f"Horizontal pair found: {column} and {column+1} in row {row_count}")
-                else:
-                    column += 1
-                    print(f"Column = {column}")
+        short_row_length = row_columns - 1
+        for value in row:
+            # print(f"Value = {value}")
+            column += 1
+            if column == short_row_length:
+                    break
+            # print(f"Column = {column}")
+            if value == coin and row[column + 1] == coin:
+                # print(f"Function has detected {value} and {row[column + 1]} are a match!")
+                # print(f"Horizontal pair found: {column} and {column+1} in row {row_count}")
+                found_horizontals[row_count] = [column, (column + 1)]
+                # print(found_horizontals)
+                if found_horizontals[row_count -1] == [column, (column+1)]:
+                    return True
+
+            else:
+                # column += 1
+                # print(f"Column = {column}")
+                pass
 
 
-        column = -1
-        print("Reset Column")
+        if found_horizontals[row_count] != [""]:
+            pass
+        else:
+            found_horizontals[row_count] = [""]
+        # print("Reset Column")
+    return False
 
-        print(f"Increased row count to: {row_count}")
+print(check_for_two([[0,0,0,"R","R"],[0,0,0,"R","R"],['R',0,0,0,0],['Y','R',0,'R','Y']], 'R'))
 
 
-check_for_two([[0,0,'R','R',0],['R','R',0,0,0],['R',0,0,0,0],['Y','R',0,'R','Y']], 'R')
 
-# check_for_two([['Y',0,0,0],['Y',0,0,0],['Y',0,'R','R'],['Y',0,'R','R']], 'R')
+print("""
+
+CHECKING SECOND BOARD
+
+""")
+print(check_for_two([[0,0,0,0,0],[0,0,0,0,0],['R',0,0,0,0],['Y','R',0,'R','Y']], 'Y'))
+
+
+print("""
+
+CHECKING Third BOARD
+
+""")
+
+print(check_for_two([['Y',0,0,0],['Y',0,0,0],['Y',0,'R','R'],['Y',0,'R','R']], 'R'))
 
 
 
